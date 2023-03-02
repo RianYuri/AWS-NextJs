889b96cf0829b36e8f44f15b572e0bb55c67e0dd
Rian Yuri <rianyurirgt@gmail.com>
2023-03-02 18:01:42 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className="text-sky-400 text-6xl font-bold underline">
+      Hello world!
+    </h1>
+    </main>
+  )
+}
